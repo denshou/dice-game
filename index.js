f996afc8a498cc4cf8833171b73a283e47cf3b41
denshou <132555762+denshou@users.noreply.github.com>
2023-08-19 17:29:46 +0900

    dice-game 로직 완성
    번갈아 가면서 주사위를 던지는 게임
    먼저 누적 점수 50점을 만들면 승리
    1,2가 나오면 현재 점수 초기화 후 턴 변경, 3,4,5,6이 나오면 현재 점수에 나온 주사위 숫자를 더한다. 홀드를 눌러 현재 점수를 누적 점수에 더할지, 계속 주사위를 던질지 선택한다.
    게임 종료시 player win 메세지가 나오고, rolldice, hold 버튼이 비활성화 된다.
    현재 순서의 플레이어 표시
    new game 버튼을 누르면 모든 점수가 초기화 된다. rolldice, hold 버튼이 비활성화 상태라면 활성화시킨다. 게임 종료 메세지가 있다면 삭제한다.

--- index.js
+++ index.js
@@ -7,56 +7,154 @@ let myTurn = 1;
 const rollButton = document.querySelector("#btn-rolldice");
 const diceImg = document.querySelector(".dice");
 const currentCount1 = document.querySelector("#player1current");
+const currentCount2 = document.querySelector("#player2current");
 
 const holdButton = document.querySelector("#btn-hold");
 const totalCount1 = document.querySelector("#player1total");
+const totalCount2 = document.querySelector("#player2total");
 
 const newGameButton = document.querySelector("#btn-newgame");
+
+const body = document.querySelector("body");
+
+const left = document.querySelector(".big-box__left");
+const right = document.querySelector(".big-box__right");
 
 function appStart() {
   rollButton.onclick = function () {
     const rand = Math.floor(Math.random() * 6 + 1);
-    if (rand === 1) {
-      diceImg.src = "assets/dice-1.png";
-      current1 = 0;
-      currentCount1.innerText = current1;
-    } else if (rand === 2) {
-      diceImg.src = "assets/dice-2.png";
-      current1 = 0;
-      currentCount1.innerText = current1;
-    } else if (rand === 3) {
-      diceImg.src = "assets/dice-3.png";
-      current1 = current1 + 3;
-      currentCount1.innerText = current1;
-    } else if (rand === 4) {
-      diceImg.src = "assets/dice-4.png";
-      current1 = current1 + 4;
-      currentCount1.innerText = current1;
-    } else if (rand === 5) {
-      diceImg.src = "assets/dice-5.png";
-      current1 = current1 + 5;
-      currentCount1.innerText = current1;
-    } else if (rand === 6) {
-      diceImg.src = "assets/dice-6.png";
-      current1 = current1 + 6;
-      currentCount1.innerText = current1;
+    if (myTurn === 1) {
+      if (rand === 1) {
+        diceImg.src = "assets/dice-1.png";
+        current1 = 0;
+        currentCount1.innerText = current1;
+        right.style.opacity = 1;
+        left.style.opacity = 0.7;
+        return (myTurn = 0);
+      } else if (rand === 2) {
+        diceImg.src = "assets/dice-2.png";
+        current1 = 0;
+        currentCount1.innerText = current1;
+        right.style.opacity = 1;
+        left.style.opacity = 0.7;
+        return (myTurn = 0);
+      } else if (rand === 3) {
+        diceImg.src = "assets/dice-3.png";
+        current1 = current1 + 3;
+        currentCount1.innerText = current1;
+      } else if (rand === 4) {
+        diceImg.src = "assets/dice-4.png";
+        current1 = current1 + 4;
+        currentCount1.innerText = current1;
+      } else if (rand === 5) {
+        diceImg.src = "assets/dice-5.png";
+        current1 = current1 + 5;
+        currentCount1.innerText = current1;
+      } else if (rand === 6) {
+        diceImg.src = "assets/dice-6.png";
+        current1 = current1 + 6;
+        currentCount1.innerText = current1;
+      }
     }
-    console.log("roll");
+
+    if (myTurn === 0) {
+      if (rand === 1) {
+        diceImg.src = "assets/dice-1.png";
+        current2 = 0;
+        currentCount2.innerText = current2;
+        right.style.opacity = 0.7;
+        left.style.opacity = 1;
+        return (myTurn = 1);
+      } else if (rand === 2) {
+        diceImg.src = "assets/dice-2.png";
+        current2 = 0;
+        currentCount2.innerText = current2;
+        right.style.opacity = 0.7;
+        left.style.opacity = 1;
+        return (myTurn = 1);
+      } else if (rand === 3) {
+        diceImg.src = "assets/dice-3.png";
+        current2 = current2 + 3;
+        currentCount2.innerText = current2;
+      } else if (rand === 4) {
+        diceImg.src = "assets/dice-4.png";
+        current2 = current2 + 4;
+        currentCount2.innerText = current2;
+      } else if (rand === 5) {
+        diceImg.src = "assets/dice-5.png";
+        current2 = current2 + 5;
+        currentCount2.innerText = current2;
+      } else if (rand === 6) {
+        diceImg.src = "assets/dice-6.png";
+        current2 = current2 + 6;
+        currentCount2.innerText = current2;
+      }
+    }
   };
 
   holdButton.onclick = function () {
-    total1 = total1 + current1;
-    totalCount1.innerText = total1;
-    current1 = 0;
-    currentCount1.innerText = current1;
+    if (myTurn === 1) {
+      total1 = total1 + current1;
+      totalCount1.innerText = total1;
+      current1 = 0;
+      currentCount1.innerText = current1;
+      right.style.opacity = 1;
+      left.style.opacity = 0.7;
+
+      if (total1 >= 50) {
+        const div = document.createElement("div");
+        div.classList.add("result");
+        div.style.cssText =
+          "position : absolute; top: 15rem; width : 20rem; height : 5rem; font-size : 2rem;  display: flex; justify-content: center; align-items: center; background-color: #d8afbe; border-radius: 10px;";
+        div.innerText = "PLAYER1 WIN!";
+        body.appendChild(div);
+        rollButton.disabled = true;
+        holdButton.disabled = true;
+      }
+
+      return (myTurn = 0);
+    }
+
+    if (myTurn === 0) {
+      total2 = total2 + current2;
+      totalCount2.innerText = total2;
+      current2 = 0;
+      currentCount2.innerText = current2;
+      right.style.opacity = 0.7;
+      left.style.opacity = 1;
+
+      if (total2 >= 50) {
+        const div = document.createElement("div");
+        div.classList.add("result");
+        div.style.cssText =
+          "position : absolute; top: 15rem; width : 20rem; height : 5rem; font-size : 2rem;  display: flex; justify-content: center; align-items: center; background-color: #d8afbe; border-radius: 10px;";
+        div.innerText = "PLAYER2 WIN!";
+        body.appendChild(div);
+        rollButton.disabled = true;
+        holdButton.disabled = true;
+      }
+      return (myTurn = 1);
+    }
   };
 
   newGameButton.onclick = function () {
+    const div = document.querySelector(".result");
     total1 = 0;
+    total2 = 0;
     totalCount1.innerText = total1;
+    totalCount2.innerText = total2;
     current1 = 0;
+    current2 = 0;
     currentCount1.innerText = current1;
+    currentCount2.innerText = current2;
+    right.style.opacity = 0.7;
+    left.style.opacity = 1;
     diceImg.src = "assets/dice-hidden.png";
+    if (div) body.removeChild(div);
+    myTurn = 1;
+    rollButton.disabled = false;
+    holdButton.disabled = false;
   };
 }
+
 appStart();
